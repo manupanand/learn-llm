3560ea93b40c930157f15bcc59372ffe0290bdc6
manupanand <manupanandindianinvestor@gmail.com>
2026-05-31 17:33:02 +0000

    pytorch learn

--- pytorch/main.py
+++ pytorch/main.py
@@ -3,4 +3,4 @@ import torch
 
 print(torch.cuda.is_available())  # return true if  cuda available  -> true
 print(torch.cuda.device_count())  # returns number of device nvidia-cuda compatiable
-print(torch.cuda.device_name(0))  # get device  name -0 index
+print(torch.cuda.get_device_name(0))  # get device  name -0 index

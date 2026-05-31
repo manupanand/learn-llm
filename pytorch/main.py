b6374935825b96ef78c1bc6da2f771ecf92bd0ec
manupanand <manupanandindianinvestor@gmail.com>
2026-05-31 17:41:47 +0000

    pytorch learn

--- pytorch/main.py
+++ pytorch/main.py
@@ -1,6 +1,13 @@
 import torch
+import numpy as np
 
 
 print(torch.cuda.is_available())  # return true if  cuda available  -> true
 print(torch.cuda.device_count())  # returns number of device nvidia-cuda compatiable
 print(torch.cuda.get_device_name(0))  # get device  name -0 index
+print(torch.cuda.device(0))  # get the device
+
+# Tensors
+# we can use numpy -numpy only run on CPU
+arr = np.array([[1,2,3],[4,5,6])
+print(arr)
